5651a37b305111fd9bf18929b37c2b2653c8b1e7
hupratt <hprattdo@gmail.com>
2020-07-07 15:47:51 +0200

    nicer modal

--- src/components/modal/PopUpModal.js
+++ src/components/modal/PopUpModal.js
@@ -1,45 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Header, Image, Modal } from "semantic-ui-react";
+import { CSSTransition } from "react-transition-group";
+import { useDispatch } from "react-redux";
+import * as actionTypes from "../../actions/actionTypes";
 
-class PopUpModal extends Component {
-  render() {
-    const { open, close, dimmer } = this.props;
+const PopUpModal = ({ open, close, dimmer }) => {
+  const dispatch = useDispatch();
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    setTimeout(() => dispatch({ type: actionTypes.RESET_FAIL }), 10000);
+  }, []);
 
-    return (
-      <div>
-        <Modal dimmer={dimmer} open={open} onClose={close}>
-          <Modal.Header>Select a Photo</Modal.Header>
-          <Modal.Content image>
-            <Image
-              wrapped
-              size="medium"
-              src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
-            />
-            <Modal.Description>
-              <Header>Default Profile Image</Header>
-              <p>
-                We've found the following gravatar image associated with your
-                e-mail address.
-              </p>
-              <p>Is it okay to use this photo?</p>
-            </Modal.Description>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button color="black" onClick={close}>
-              Nope
-            </Button>
-            <Button
-              positive
-              icon="checkmark"
-              labelPosition="right"
-              content="Yep, that's me"
-              onClick={close}
-            />
-          </Modal.Actions>
-        </Modal>
-      </div>
-    );
-  }
-}
+  return (
+    <CSSTransition
+      in={visible}
+      classNames="fadeout"
+      unmountOnExit
+      timeout={500}
+    >
+      <Modal dimmer={dimmer} open={open} onClose={close}>
+        <Modal.Header>Select a Photo</Modal.Header>
+        <Modal.Content image>
+          <Image
+            wrapped
+            size="medium"
+            src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
+          />
+          <Modal.Description>
+            <Header>Default Profile Image</Header>
+            <p>
+              We've found the following gravatar image associated with your
+              e-mail address.
+            </p>
+            <p>Is it okay to use this photo?</p>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="black" onClick={close}>
+            Nope
+          </Button>
+          <Button
+            positive
+            icon="checkmark"
+            labelPosition="right"
+            content="Yep, that's me"
+            onClick={close}
+          />
+        </Modal.Actions>
+      </Modal>
+    </CSSTransition>
+  );
+};
 
 export default PopUpModal;
