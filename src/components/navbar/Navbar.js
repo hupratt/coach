11f2da6938f70e21768510bcf6baf5ebf4c711da
hupratt <hprattdo@gmail.com>
2020-07-12 13:55:06 +0200

    new navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,11 +5,34 @@ import homeIcon from "../../images/homeIcon.png";
 function Navbar() {
   return (
     <div className="navbar-wrapper">
-      <div className="navbar">
-        <button className="icon board-icon-wrapper">
-          <i className="fas fa-user-circle" /> Profile
-        </button>
+      <div className="nav-container">
+        {/* <div className="navbar-search">
+            <input
+              type="search"
+              className="navbar-search__input"
+              placeholder="Search"
+            />
+          </div> */}
+
+        <div className="navbar-profile">
+          <div className="navbar-profile__picture">
+            <img
+              src="https://images.unsplash.com/photo-1505150892987-424388901632?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+              alt="Profile Picture"
+            />
+          </div>
+          <div className="navbar-profile__name">Mike D.</div>
+        </div>
       </div>
+
+      {/* <div className="profile">
+        <i className="fab fa-codepen" id="codepen"></i>
+        <p className="author">
+          <a href="https://codepen.io/#" target="_blank">
+            /hpratt
+          </a>
+        </p>
+      </div> */}
     </div>
   );
 }
