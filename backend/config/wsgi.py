66510d2102798743532f359e45796765695cd395
hupratt <hprattdo@gmail.com>
2020-07-16 09:06:23 +0200

    missing wsgi conf

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -7,10 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys, dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+
+application = get_wsgi_application()
+
+
+if os.environ.get("DJANGO_DEVELOPMENT") == "true":
+    dotenv.read_dotenv(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+            ".env.development",
+        )
+    )
+else:
+    dotenv.read_dotenv(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env"
+        )
+    )
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings")
 
 application = get_wsgi_application()
