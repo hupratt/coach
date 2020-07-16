2b0a3cb82c1d8b535804e07cfe6ad9a9424ebcc0
hupratt <hprattdo@gmail.com>
2020-07-16 09:23:43 +0200

    better settings

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,37 +1,19 @@
 import os
 from .base import *
-from .base import BASE_DIR
-from ..env_utils import get_env
 
 DEBUG = False
-SECRET_KEY = get_env("DJANGO_SECRET_KEY")
-STATIC_URL = get_env("DJANGO_STATIC_URL")
-STATIC_ROOT = get_env("DJANGO_STATIC_ROOT")
-ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS").split(",")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
+STATIC_URL = os.environ.get("STATIC_URL", "")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "")
 
 # Nginx is used instead of SecurityMiddleware
 # for setting all the recommended security headers
 SILENCED_SYSTEM_CHECKS = [
     "security.W001",
 ]
-ALLOWED_HOSTS = []
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "PORT": get_env("POSTGRES_PORT"),
-#         "HOST": get_env("POSTGRES_HOST"),
-#         "NAME": get_env("POSTGRES_DB"),
-#         "USER": get_env("POSTGRES_USER"),
-#         "PASSWORD": get_env("POSTGRES_PASSWORD"),
-#         "ATOMIC_REQUESTS": True,
-#     }
-# }
-
-
-# TODO: Add proper handlers
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
