4e36f8559dfa5f3275df84ae71591355e5436002
hupratt <hprattdo@gmail.com>
2020-07-16 08:59:01 +0200

    fix prod config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -3,17 +3,17 @@ const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   watch: false,
-  entry: "./ecom/frontend/src/index.js",
+  entry: "./backend/frontend/src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "ecom/frontend/static/frontend")
+    path: path.resolve(__dirname, "backend/frontend/static/frontend"),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ["babel-loader"],
       },
       {
         test: /\.s?css$/,
@@ -21,39 +21,39 @@ module.exports = {
           "style-loader",
           {
             loader: "css-loader",
-            options: { sourceMap: true }
+            options: { sourceMap: true },
           },
           {
             loader: "sass-loader",
-            options: { sourceMap: true }
-          }
-        ]
+            options: { sourceMap: true },
+          },
+        ],
       },
       {
         test: /\.(jpe?g|png|gif|woff2|woff|eot|ttf|otf|svg)$/,
         use: [
           {
             loader: "url-loader",
-            options: { limit: 2500 }
-          }
-        ]
-      }
-    ]
+            options: { limit: 2500 },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
     modules: [path.join(__dirname, "src"), "node_modules"],
-    extensions: ["*", ".js", ".scss"]
+    extensions: ["*", ".js", ".scss"],
   },
   devtool: "eval-source-map",
   devServer: {
     contentBase: path.join(__dirname, "dev"),
     historyApiFallback: true,
-    hot: true
+    hot: true,
   },
   plugins: [
     new Dotenv({
       safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
-      silent: true // hide any errors
-    })
-  ]
+      silent: true, // hide any errors
+    }),
+  ],
 };
