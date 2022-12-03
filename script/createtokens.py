c4126b99e90d3fd3914b71462359c1d5d456d464
Hugo Pratt <hprattdo@gmail.com>
2022-12-03 20:12:30 +0100

    fix model and remove background slowing down the app

--- script/createtokens.py
+++ script/createtokens.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-
 User = get_user_model()
 for user in User.objects.all():
     Token.objects.get_or_create(user=user)
+
