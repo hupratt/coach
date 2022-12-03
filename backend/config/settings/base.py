689562f0abcfc67d11ec1c1f20e78b829338619f
Hugo Pratt <hprattdo@gmail.com>
2022-12-03 22:17:26 +0100

    fix

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -110,13 +110,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # }
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "coach",
-        "USER": os.environ.get("dbuser"),
-        "PASSWORD": os.environ.get("dbpassword"),
-        "HOST": os.environ.get("hostip"),
-        "PORT": os.environ.get("pnumber"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
