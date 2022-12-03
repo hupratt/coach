c0bc027bf77368bfb84b8892315e7cab018ac51b
Hugo Pratt <hprattdo@gmail.com>
2022-12-03 21:52:24 +0100

    try debug psycop error

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "coach",
         "USER": os.environ.get("dbuser"),
         "PASSWORD": os.environ.get("dbpassword"),
